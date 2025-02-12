48eb9429214c885880327ffa4266ff8b54770c47
kurak57 <tomitimotius92@gmail.com>
2025-02-12 21:54:11 +0700

    day 24 mail merge update

--- day_24/main.py
+++ day_24/main.py
@@ -1,19 +1,9 @@
-#TODO: Create a letter using starting_letter.txt 
-
-
-names = open("./Input/Names/invited_names.txt", "r")
-with open("./Input/Letters/starting_letter.txt") as file:
-    letter = file.read()
-    for name in names.readlines():
+with open("./Input/Names/invited_names.txt", "r") as names_file:
+    names = names_file.readlines()
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter = letter_file.read()
+    for name in names:
         clean_name = name.strip()
         new_letter = letter.replace("[name]", clean_name)
-        with open(f"./Output/ReadyToSend/letter_for_{clean_name}.txt", "w") as file:
-            file.write(new_letter)
-
-names.close()
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+        with open(f"./Output/ReadyToSend/letter_for_{clean_name}.txt", "w") as invit_letter:
+            invit_letter.write(new_letter)
